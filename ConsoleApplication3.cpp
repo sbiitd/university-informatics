82cbbe722d45526ed55c8309e0a4428f9f632e4c
sbiitd <demoonl.10@yandex.ru>
2025-10-02 13:52:32 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    float x, y;
+
+    cout << "Введите x: ";
+    cin >> x;
+    cout << "Введите y: ";
+    cin >> y;
+
+    float R = pow(1.0 / cos(sqrt(y)), 3); 
+    float S = sqrt(fabs(x)) / log(y);
+
+    cout << "R = " << R << endl;
+    cout << "S = " << S << endl;
+
+    float C = max(R, S);
+
+    cout << "C = " << C << endl;
+    system("pause");
+
+    return 0;
+}
